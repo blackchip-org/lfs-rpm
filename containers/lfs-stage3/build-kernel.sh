4f0f5665fb2b491eeab51bbdb28ed0773c2917d8
Mike McGann <mike.mcgann@blackchip.org>
2023-10-15 20:39:23 -0400

    boot wip

---
+++ containers/lfs-stage3/build-kernel.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+
+set -e -x 
+
+lfs_version="12.0"
+kernel_version="6.4.12" 
+
+basedir=/home/lfs/kernel 
+
+srcdir="$basedir/linux"
+bootdir="$basedir/boot"
+rootdir="$basedir/root" 
+
+if [ ! -d "$srcdir" ] ; then 
+    mkdir -p "$srcdir" 
+    tar xf rpmbuild/SOURCES/linux-*.tar.xz -C "$srcdir" --strip-components=1 
+fi 
+
+cd "$srcdir"
+if [ ! -e .config ] ; then 
+    make mrproper   
+    make defconfig 
+fi 
+
+make 
+
+mkdir -p "$rootdir"
+make INSTALL_MOD_PATH=${rootdir} modules_install 
+
+mkdir -p "$bootdir"
+cp -v arch/x86/boot/bzImage "$bootdir/vmlinuz-${kernel_version}-lfs-${lfs_version}"
+cp -v System.map "$bootdir/System.map-$kernel_version"
+cp -v .config "$bootdir/config-6.4.12"
+
+install -v -m755 -d "${rootdir}/etc/modprobe.d"
+cat > "$rootdir/etc/modprobe.d/usb.conf" <<EOF
+install ohci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i ohci_hcd ; true
+install uhci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i uhci_hcd ; true
+EOF
+
+cd "$bootdir/.."
+tar zcf boot-lfs-${lfs_version}.tar.gz "$(basename $bootdir)"
+
+cd "$rootdir"
+tar zcf /home/lfs/modules-lfs-${lfs_version}.tar.gz * 
+
